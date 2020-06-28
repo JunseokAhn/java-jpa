0cf38ca2f0a92ef4e2d310d339824387f8036255
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-06-29 04:23:23 +0900

    Add DB autoCreate

--- src/main/java/VO/Member.java
+++ src/main/java/VO/Member.java
@@ -1,5 +1,6 @@
 package VO;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -10,7 +11,9 @@ public class Member {
     @Id
     private long id;
 
-//    @Column(name = "user_name") user_name이라는 컬럼으로 쿼리가 나가도록 변경
+//    @Column(name = "user_name", unique = true, length = 10, nullable = false)
+//    user_name이란 컬럼으로 길이10, not null, unique옵션 붙여서 create/update
+//    DB자동생성 옵션이 활성화되어있을떄 적용(create랑 update말고는 저 옵션이 활용될수없기때문)
     private String name;
 
     public Member(){}
