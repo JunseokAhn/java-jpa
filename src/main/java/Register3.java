9bd0ee0f54b90d64640332e392f94c36e549f709
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-07-06 15:13:03 +0900

    Add IDENTITY ( PRIMARY KEY Mapping )

--- src/main/java/Register3.java
+++ src/main/java/Register3.java
@@ -20,12 +20,8 @@ public class Register3 {
             Member3 member = new Member3();
             //soutv
             System.out.println("====================");
-            /*identity로 쿼리를 날리게되면, 하이버네이트입장에선 값을 insert로 날려봐야=
-                    pk를 알수있기때문에 영속성컨텍스트에 집어넣는순간 바로 insert를날린다. 라고하는데,
-                    실제로는 자동생성된 시퀸스의 next val을 select해버린다.
-            아마도 identity는 알아서 db에서 시퀀스를 만들어주기떄문에 insert해봐야 값을 알지만
-                    이경우, 내가 GeneratedValue로 시퀀스를 생성해주었기때문인거같음. 따라서 identity가아니라
-                    sequence를 사용하는것이 맞다.*/
+            //identity로 쿼리를 날리게되면, 하이버네이트입장에선 값을 insert로 날려봐야
+            //pk를 알수있기때문에 영속성컨텍스트에 집어넣는순간 바로 insert를날린다.
             EM.persist(member);
             System.out.println("member.id = " + member.getId());
             System.out.println("====================");
